d500aa6b43e608be6265e082f308df540b0565c4
Alejo Mendoza <alejandro@skyhitzmusic.com>
2022-07-11 09:31:50 -0500

    init

---
+++ packages/app/provider/index.tsx
@@ -0,0 +1,13 @@
+import { Dripsy } from './dripsy'
+import { NavigationProvider } from './navigation'
+import { RecoilRoot } from 'recoil';
+
+export function Provider({ children }: { children: React.ReactNode }) {
+  return (
+    <NavigationProvider>
+      <RecoilRoot>
+        <Dripsy>{children}</Dripsy>
+      </RecoilRoot>
+    </NavigationProvider>
+  )
+}
