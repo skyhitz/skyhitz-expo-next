0780fb05e93eae6a30491cbcbf96330cd2143e7b
Alejo Mendoza <alejandro@skyhitzmusic.com>
2022-07-27 09:32:49 -0500

    add walletconnect v2

--- packages/app/state/wallet-connect.ts
+++ packages/app/state/wallet-connect.ts
@@ -40,7 +40,7 @@ const sessionAtom = atom<any>({
 })
 
 export const WalletConnectStore = () => {
-  const [client, setClient] = useRecoilState(clientAtom)
+  let client: WalletConnect
   const [uri, setUri] = useRecoilState(uriAtom)
   const [state, setState] = useRecoilState(stateAtom)
   const [publicKey, setPublicKey] = useRecoilState(publicKeyAtom)
@@ -48,7 +48,7 @@ export const WalletConnectStore = () => {
   console.log('wallet connect store')
   const init = async () => {
     if (client) return
-    const result = await WalletConnect.init({
+    client = await WalletConnect.init({
       projectId: '422a527ddc3ed4c5fff60954fcc8ed83',
       metadata: {
         name: 'Skyhitz',
@@ -57,12 +57,7 @@ export const WalletConnectStore = () => {
         icons: ['https://skyhitz.io/img/icon-512.png'],
       },
     })
-    console.log(result)
-    // setClient(result)
-    // if (result.session) {
-    //   handleSetSession(result.session)
-    // }
-    // subscribeToEvents()
+    subscribeToEvents()
   }
 
   useEffect(() => {
@@ -70,104 +65,109 @@ export const WalletConnectStore = () => {
   })
 
   const clearState = () => {
-    // setState('disconnected')
-    // setPublicKey('')
-    // handleSetSession(null)
-    // setClient(null)
+    setState('disconnected')
+    setPublicKey('')
+    onSessionUpdate(null)
   }
 
-  const handleSetSession = (session) => {
-    // setSession(session)
-    // const { state } = session
-    // const [stellarAccount] = state.accounts
-    // setPublicKey(stellarAccount.replace(`${stellarMeta.chainName}:`, ''))
-    // setState('session-created')
-    // return publicKey
+  const onSessionUpdate = (session) => {
+    setSession(session)
+    const { state } = session
+    const [stellarAccount] = state.accounts
+    setPublicKey(stellarAccount.replace(`${stellarMeta.chainName}:`, ''))
+    setState('session-created')
+    return publicKey
   }
 
   const connect = async () => {
-    // if (!client) return
+    if (!client) return
     try {
-      // const { uri, approval } = await client.connect({
-      //   requiredNamespaces: {
-      //     stellar: {
-      //       chains: [stellarMeta.chainName],
-      //       methods: stellarMeta.methods,
-      //       events: ['chainChanged', 'accountsChanged'],
-      //     },
-      //   },
-      // })
-      // if (!uri) return
-      // setUri(uri)
-      // setState('paring-proposal')
-      // const session = await approval()
-      // handleSetSession(session)
-      // setState('paring-created')
-      // setUri(null)
+      const { uri, approval } = await client.connect({
+        requiredNamespaces: {
+          stellar: {
+            methods: stellarMeta.methods,
+            chains: [stellarMeta.chainName],
+            events: ['chainChanged', 'accountsChanged'],
+          },
+        },
+      })
+      if (!uri) return
+      setUri(uri)
+      setState('paring-proposal')
+      const session = await approval()
+      onSessionUpdate(session)
+      setState('paring-created')
+      setUri(null)
     } catch (e) {
       console.log('catched error on reject:', e)
-      // setState('disconnected')
+      setState('disconnected')
     }
 
-    // return publicKey
+    return publicKey
   }
 
   const disconnect = async () => {
-    // await client?.disconnect({
-    //   topic: session.topics[0],
-    //   reason: {
-    //     code: 1,
-    //     message: 'Logged out',
-    //   },
-    // })
+    await client?.disconnect({
+      topic: session.topic,
+      reason: {
+        code: 1,
+        message: 'Logged out',
+      },
+    })
     await clearState()
   }
 
   const subscribeToEvents = () => {
-    console.log('subscribed to events')
+    if (!client) return
 
-    // client?.on('session_proposal', async (proposal) => {
-    //   setState('session-proposal')
-    // })
+    client.on('session_proposal', async (proposal) => {
+      setState('session-proposal')
+    })
 
-    // client?.on('session_update', async (proposal) => {
-    //   setState('session-created')
-    // })
+    client.on('session_update', ({ topic, params }) => {
+      const { namespaces } = params
+      const _session = client.session.get(topic)
+      // Overwrite the `namespaces` of the existing session with the incoming one.
+      const updatedSession = { ..._session, namespaces }
+      // Integrate the updated session state into your dapp state.
+      onSessionUpdate(updatedSession)
+      setState('session-created')
+    })
 
-    // client?.on('session_delete', (session) => {
-    //   console.log(session)
-    //   clearState()
-    // })
+    client.on('session_delete', (session) => {
+      console.log(session)
+      clearState()
+    })
   }
 
   const signXdr = (xdr) => {
-    // if (!client) return
-    // return client.request({
-    //   topic: session.topics[0],
-    //   chainId: stellarMeta.chainName,
-    //   request: {
-    //     jsonrpc: '2.0',
-    //     method: 'stellar_signXDR',
-    //     params: {
-    //       xdr,
-    //     },
-    //   } as any,
-    // })
+    if (!client) return
+    return client.request({
+      topic: session.topic,
+      chainId: stellarMeta.chainName,
+      request: {
+        jsonrpc: '2.0',
+        method: 'stellar_signXDR',
+        params: {
+          xdr,
+        },
+      } as any,
+    })
   }
 
   const signAndSubmitXdr = (xdr) => {
-    // if (!client) return
-    // return client.request({
-    //   topic: session.topics[0],
-    //   chainId: stellarMeta.chainName,
-    //   request: {
-    //     jsonrpc: '2.0',
-    //     method: 'stellar_signAndSubmitXDR',
-    //     params: {
-    //       xdr,
-    //     },
-    //   } as any,
-    // })
+    if (!client) return
+    return client.request({
+      topic: session.topic,
+      chainId: stellarMeta.chainName,
+      request: {
+        jsonrpc: '2.0',
+        method: 'stellar_signAndSubmitXDR',
+        params: {
+          xdr,
+        },
+      } as any,
+    })
   }
 
   return {
@@ -178,9 +178,9 @@ export const WalletConnectStore = () => {
     connect,
     clearState,
     init,
-    // publicKey,
-    // uri,
-    // session,
-    // state,
+    publicKey,
+    uri,
+    session,
+    state,
   }
 }
--- packages/app/ui/walletconnect-btn.tsx
+++ packages/app/ui/walletconnect-btn.tsx
@@ -1,39 +1,35 @@
 import React, { useEffect } from 'react'
 import WalletConnectIcon from 'app/ui/icons/walletconnect-icon'
-// import QRCodeModal from '@walletconnect/qrcode-modal'
+import QRCodeModal from '@walletconnect/qrcode-modal'
 import { signManageDataOp } from 'app/stellar'
 import { WalletConnectStore } from 'app/state/wallet-connect'
 import { Button } from 'app/design-system/button'
 import { View } from 'app/design-system'
 
 const WalletConnectBtn = ({ signInWithXDR }: { signInWithXDR?: (_) => {} }) => {
-  console.log('init btn')
-  // const state = 'session-proposal'
   const { uri, signXdr, publicKey, connect, state } = WalletConnectStore()
 
-  // useEffect(() => {
-  //   if (!uri) return QRCodeModal.close()
-  //   QRCodeModal.open(uri, () => {}, {
-  //     desktopLinks: [],
-  //     mobileLinks: ['lobstr'],
-  //   })
-  // }, [uri])
+  useEffect(() => {
+    if (!uri) return QRCodeModal.close()
+    QRCodeModal.open(uri, () => {}, {
+      desktopLinks: [],
+      mobileLinks: ['lobstr'],
+    })
+  }, [uri])
 
   const handleSignInWithXdr = async (publicKey: string) => {
     const xdr = await signManageDataOp(publicKey)
-    // const { signedXDR } = await (signXdr(xdr) as Promise<{ signedXDR: string }>)
-    // signInWithXDR && signInWithXDR(signedXDR)
+    const { signedXDR } = await (signXdr(xdr) as Promise<{ signedXDR: string }>)
+    signInWithXDR && signInWithXDR(signedXDR)
   }
 
-  const handleConnect = () => {}
-
-  // useEffect(() => {
-  //   if (publicKey && signInWithXDR) handleSignInWithXdr(publicKey)
-  // }, [publicKey])
+  useEffect(() => {
+    if (publicKey && signInWithXDR) handleSignInWithXdr(publicKey)
+  }, [publicKey])
 
   return (
     <Button
-      onPress={handleConnect}
+      onPress={connect}
       text={
         state === 'session-proposal'
           ? 'Waiting for approval'
