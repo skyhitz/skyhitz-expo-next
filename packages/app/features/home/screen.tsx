d84d406bd9e96dde5ae35c4815d6f0098e28648b
co012 <kacper.slusarczyk@swmansion.com>
2022-08-08 19:11:42 +0200

    IMPROVE: screen styling

--- packages/app/features/home/screen.tsx
+++ packages/app/features/home/screen.tsx
@@ -1,13 +1,34 @@
 import Navbar from 'app/ui/navbar'
 import BackgroundImage from 'app/ui/background-image'
 import Footer from 'app/ui/footer'
+import { Button, Text, View } from 'app/design-system'
 
 export function HomeScreen() {
   return (
-    <BackgroundImage>
+    <View tw="w-full h-full">
+      <BackgroundImage />
       <Navbar />
-      {/* <MainWrapper /> */}
+      <View tw="mt-16 mx-auto text-center" sx={{ width: [300, 400] }}>
+        <Text
+          tw="mt-4 text-center"
+          sx={{
+            fontSize: [36, 64],
+            lineHeight: [48, 56],
+            fontFamily: 'Raleway-Light',
+          }}
+        >
+          Music NFTs on Stellar
+        </Text>
+        <Text
+          tw="mt-4 text-center mb-9 leading-6"
+          sx={{ fontFamily: 'Raleway-Light' }}
+        >
+          Upload, buy or sell music NFTs on the Stellar Network. Join a
+          community of beatmakers!
+        </Text>
+        <Button tw="mt-4 text-lg" text="Sign up for free" onPress={() => {}} />
+      </View>
       <Footer />
-    </BackgroundImage>
+    </View>
   )
 }
