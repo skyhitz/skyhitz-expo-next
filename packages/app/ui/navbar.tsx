a4e5b44a375032d6564505e5ee544ec8943d836f
co012 <kacper.slusarczyk@swmansion.com>
2022-08-09 12:40:05 +0200

    IMPROVE: navbar styling

--- packages/app/ui/navbar.tsx
+++ packages/app/ui/navbar.tsx
@@ -7,21 +7,32 @@ const Navbar = () => {
   const user = useRecoilValue(userAtom)
 
   return (
-    <View tw="absolute top-0 border-black h-14 flex-row justify-between items-center w-full">
+    <View tw="flex-row justify-between items-center p-3 w-full flex-wrap">
       <TextLink href="/" tw="flex-row justify-start items-center">
-        <View tw="flex-row items-center pl-3">
+        <View tw="flex-row items-center">
           <SkyhitzLogo />
-          <Text sx={{ letterSpacing: 12 }}>SKYHITZ</Text>
+          <Text tw="pl-5 text-lg font-raleway" sx={{ letterSpacing: 12 }}>
+            SKYHITZ
+          </Text>
         </View>
       </TextLink>
       {user ? null : (
-        <View tw="flex-row justify-end items-center pr-5">
-          <TextLink tw="p-2" href="/sign-in">
+        <View tw="flex-row justify-end items-center">
+          <TextLink
+            tw="font-raleway text-sm font-bold tracking-0.5 mr-4"
+            href="/sign-in"
+          >
             Log in
           </TextLink>
-          <TextLink tw="p-2" href="/sign-up">
-            Sign Up
-          </TextLink>
+
+          <View tw="bg-white rounded-full px-3">
+            <TextLink
+              tw="p-2 font-raleway text-sm text-black font-bold tracking-0.5"
+              href="/sign-up"
+            >
+              Sign Up
+            </TextLink>
+          </View>
         </View>
       )}
     </View>
