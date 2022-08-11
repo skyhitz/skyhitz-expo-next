bd058b41333be506275f00a4cc50a64a69d0db52
co012 <kacper.slusarczyk@swmansion.com>
2022-08-11 11:17:22 +0200

    ADD: link tw with device info

--- packages/app/provider/index.tsx
+++ packages/app/provider/index.tsx
@@ -3,8 +3,11 @@ import { NavigationProvider } from './navigation'
 import { RecoilRoot } from 'recoil'
 import { FontProvider } from './font'
 import { SkyhitzApolloProvider } from './apollo'
+import { useDeviceContext } from 'twrnc'
+import { tw } from 'app/design-system/tailwind'
 
 export function Provider({ children }: { children: React.ReactNode }) {
+  useDeviceContext(tw)
   return (
     <SkyhitzApolloProvider>
       <NavigationProvider>
