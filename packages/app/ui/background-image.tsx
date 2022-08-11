39a54b3d0bd7b6a2059c2f134acf4f7ee4066483
co012 <kacper.slusarczyk@swmansion.com>
2022-08-11 13:22:09 +0200

    FIX: background image overlay not covering full width

--- packages/app/ui/background-image.tsx
+++ packages/app/ui/background-image.tsx
@@ -18,8 +18,11 @@ const BackgroundImage = () => (
     <ImageBackground
       source={SliderOverlay}
       style={{
-        width: '100%',
-        height: '100%',
+        position: 'absolute',
+        left: 0,
+        top: 0,
+        right: 0,
+        bottom: 0,
       }}
       imageStyle={{ opacity: 0.9 }}
       resizeMode="repeat"
