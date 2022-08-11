73e0293b1b1b6f698dae9351a18c375e7f76008c
co012 <kacper.slusarczyk@swmansion.com>
2022-08-11 11:19:14 +0200

    IMPROVE: show Sign in btn on mobile

--- packages/app/features/home/screen.tsx
+++ packages/app/features/home/screen.tsx
@@ -2,37 +2,25 @@ import Navbar from 'app/ui/navbar'
 import BackgroundImage from 'app/ui/background-image'
 import Footer from 'app/ui/footer'
 import { Pressable, Text, View } from 'app/design-system'
+import { useLink } from 'solito/link'
 
 export function HomeScreen() {
   return (
-    <View tw="w-full h-full flex flex-col h-full">
+    <View className="w-full h-full flex flex-col h-full">
       <BackgroundImage />
       <Navbar />
-      <View
-        tw="mx-auto text-center flex-1"
-        sx={{ width: [300, 600], mt: [24, 42] }}
-      >
-        <Text
-          tw="mt-4 text-center font-raleway"
-          sx={{
-            fontSize: [36, 64],
-            lineHeight: [40, 76],
-          }}
-        >
+      <View className="mx-auto text-center flex-1 max-w-[300px] mt-6 sm:max-w-[600px] sm:mt-10">
+        <Text className="mt-4 text-center font-raleway text-4xl sm:text-[4rem] sm:leading-[4.75rem]">
           Music NFTs on Stellar
         </Text>
-        <Text
-          tw="mx-auto text-center mb-9 max-w-[500px] font-raleway font-semibold tracking-0.5"
-          sx={{
-            fontSize: [16, 18],
-            lineHeight: 24,
-            marginTop: [38, 32],
-          }}
-        >
+        <Text className="mx-auto text-center mb-9 max-w-[500px] font-raleway font-semibold tracking-0.5 text-sm leading-6 mt-9 sm:text-base sm:mt-8">
           Upload, buy or sell music NFTs on the Stellar Network. Join a
           community of beatmakers!
         </Text>
-        <SignUpButton />
+        <View className="android:w-60 ios:w-60 mx-auto">
+          <SignUpButton />
+          <LogInButton />
+        </View>
       </View>
       <Footer />
     </View>
@@ -41,9 +29,23 @@ export function HomeScreen() {
 
 function SignUpButton() {
   return (
-    <Pressable tw="mx-auto bg-white border-2 border-black rounded-full py-3 px-8">
-      <Text tw="text-black text-lg font-raleway font-medium">
+    <Pressable className="btn bg-white border-2 border-black">
+      <Text className="text-black text-lg font-raleway font-medium leading-none">
         Sign up for free
+      </Text>
+    </Pressable>
+  )
+}
+
+function LogInButton() {
+  const logInBtnProps = useLink({ href: '/log-in' })
+  return (
+    <Pressable
+      className="btn w-full mt-2 sm:hidden border-2 border-black"
+      {...logInBtnProps}
+    >
+      <Text className="text-lg text-center font-raleway font-medium leading-none">
+        Sign in
       </Text>
     </Pressable>
   )
