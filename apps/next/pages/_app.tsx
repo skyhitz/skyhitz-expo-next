d217991ea41324819294e5c0659c1552480510be
co012 <kacper.slusarczyk@swmansion.com>
2022-08-12 09:07:12 +0200

    IMPROVE: move font stuff to font provider

--- apps/next/pages/_app.tsx
+++ apps/next/pages/_app.tsx
@@ -3,7 +3,6 @@ import Head from 'next/head'
 import React from 'react'
 import type { SolitoAppProps } from 'solito'
 import 'raf/polyfill'
-import '../style.css'
 
 function MyApp({ Component, pageProps }: SolitoAppProps) {
   return (
@@ -15,12 +14,6 @@ function MyApp({ Component, pageProps }: SolitoAppProps) {
           content="Expo + Next.js with Solito. By Fernando Rojo."
         />
         <link rel="icon" href="/favicon.ico" />
-        <link
-          rel="preload"
-          href="/fonts/Raleway-Light.ttf"
-          as="font"
-          crossOrigin=""
-        />
       </Head>
       <Provider>
         <Component {...pageProps} />
--- packages/app/provider/font/index.web.tsx
+++ packages/app/provider/font/index.web.tsx
@@ -1,5 +1,26 @@
 // on Web, we skip loading fonts
 
+import Head from 'next/head'
+import React from 'react'
+
 export const FontProvider = ({ children }: { children: React.ReactNode }) => {
-  return <>{children}</>
+  return (
+    <>
+      <Head>
+        <link
+          rel="preload"
+          href="/fonts/Raleway-Light.ttf"
+          as="font"
+          crossOrigin=""
+        />
+        <style>{`
+         @font-face {
+         font-family: 'Raleway-Light';
+         src: url('/fonts/Raleway-Light.ttf');
+         }
+       `}</style>
+      </Head>
+      {children}
+    </>
+  )
 }
