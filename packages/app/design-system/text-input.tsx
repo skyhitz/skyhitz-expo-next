3e916918d37ef7bfac95e57e2e9e44b99b5835cb
co012 <kacper.slusarczyk@swmansion.com>
2022-08-17 11:44:36 +0200

    IMPROVE: cleaner code

--- packages/app/design-system/text-input.tsx
+++ packages/app/design-system/text-input.tsx
@@ -1,18 +1,16 @@
 import { ComponentProps } from 'react'
 import { TextInput as DripsyTextInput } from 'dripsy'
-import { Platform } from 'react-native'
 
 import { tw as tailwind } from 'app/design-system/tailwind'
 
-type TextProps = {
+export type TextProps = {
   className?: string
 } & ComponentProps<typeof DripsyTextInput>
 
 function TextInput({ className, sx, ...props }: TextProps) {
   return (
     <DripsyTextInput
-      sx={{ ...sx, ...tailwind.style(className) }}
-      style={Platform.OS === 'web' ? ({ outlineStyle: 'none' } as any) : {}}
+      sx={{ ...sx, ...tailwind.style(className, 'web:outline-none') }}
       {...props}
     />
   )
