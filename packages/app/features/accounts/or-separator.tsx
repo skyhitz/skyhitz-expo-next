fc79dcca78f01e4a8c74562c7968b0103dc9ef0c
co012 <kacper.slusarczyk@swmansion.com>
2022-08-17 13:05:16 +0200

    IMPROVE: or-separator

--- packages/app/features/accounts/or-separator.tsx
+++ packages/app/features/accounts/or-separator.tsx
@@ -1,13 +1,15 @@
 import { Text, View } from 'app/design-system'
 
-const LINE_STYLE = 'border border-transparent border-b-white grow'
+const Line = () => (
+  <View className="border border-transparent border-b-white grow" />
+)
 
 export function Separator() {
   return (
     <View className="flex flex-row my-8 items-center w-full">
-      <View className={LINE_STYLE} />
+      <Line />
       <Text className="px-2">or</Text>
-      <View className={LINE_STYLE} />
+      <Line />
     </View>
   )
 }
