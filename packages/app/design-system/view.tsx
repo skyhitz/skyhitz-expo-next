b0f08b35e57f37d5659bf78361d97a6a82ef404c
co012 <kacper.slusarczyk@swmansion.com>
2022-08-24 16:27:51 +0200

    IMPROVE: apply prettier

--- packages/app/design-system/view.tsx
+++ packages/app/design-system/view.tsx
@@ -2,7 +2,9 @@ import { ComponentProps } from 'react'
 import { useSx, View as DripsyView } from 'dripsy'
 import { tw as tailwind } from 'app/design-system/tailwind'
 
-type ViewProps = { className?: string } & ComponentProps<typeof DripsyView>
+export type ViewProps = { className?: string } & ComponentProps<
+  typeof DripsyView
+>
 
 function View({ className, sx, ...props }: ViewProps) {
   useSx()
