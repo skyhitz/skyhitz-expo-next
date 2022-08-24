e16dec7310381e917ed09cef75792489fd0e19f6
co012 <kacper.slusarczyk@swmansion.com>
2022-08-24 11:41:05 +0200

    ADD: styling ability

--- packages/app/ui/icons/dollar.tsx
+++ packages/app/ui/icons/dollar.tsx
@@ -1,9 +1,23 @@
 import React from 'react'
-import Svg, { Path, G } from 'react-native-svg'
+import Svg, { G, Path } from 'react-native-svg'
+import { tw } from 'app/design-system/tailwind'
 
-function Icon({ size = 24, color }) {
+function Icon({
+  size = 24,
+  color,
+  className,
+}: {
+  size: number
+  color: string
+  className?: string
+}) {
   return (
-    <Svg width={size} height={size} viewBox="0 0 236.36 200">
+    <Svg
+      width={size}
+      height={size}
+      viewBox="0 0 236.36 200"
+      style={tw.style(className)}
+    >
       <G id="Layer_2" data-name="Layer 2">
         <G id="Layer_1-2" data-name="Layer 1" fill={color ? color : 'white'}>
           <Path d="M203,26.16l-28.46,14.5-137.43,70a82.49,82.49,0,0,1-.7-10.69A81.87,81.87,0,0,1,158.2,28.6l16.29-8.3,2.43-1.24A100,100,0,0,0,18.18,100q0,3.82.29,7.61a18.19,18.19,0,0,1-9.88,17.58L0,129.57V150l25.29-12.89,0,0,8.19-4.18,8.07-4.11v0L186.43,55l16.28-8.29,33.65-17.15V9.14Z" />
