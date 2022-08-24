cd95a5c43d36a16382581b948cbf05f145576e9b
co012 <kacper.slusarczyk@swmansion.com>
2022-08-24 16:59:01 +0200

    FIX: link

--- packages/app/ui/navbar.tsx
+++ packages/app/ui/navbar.tsx
@@ -8,7 +8,7 @@ const Navbar = () => {
 
   return (
     <View className="flex-row justify-center sm:justify-between items-center p-3 w-full flex-wrap">
-      <TextLink href="/" tw="flex flex-row justify-start items-center">
+      <TextLink href="/home" tw="flex flex-row justify-start items-center">
         <View className="flex flex-row items-center">
           <SkyhitzLogo />
           <Text className="pl-4 text-lg font-raleway tracking-[12px]">
