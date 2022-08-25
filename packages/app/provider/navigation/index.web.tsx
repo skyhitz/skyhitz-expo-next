40034dd015fee106af8420c39c80f3b4b329a903
co012 <kacper.slusarczyk@swmansion.com>
2022-08-25 08:45:39 +0200

    IMPROVE: display dashboard navigation only on dashboard

--- packages/app/provider/navigation/index.web.tsx
+++ packages/app/provider/navigation/index.web.tsx
@@ -1,25 +1,15 @@
-// on Web, we don't use React Navigation, so we avoid the provider altogether
-// instead, we just have a no-op here
-// for more, see: https://solito.dev/recipes/tree-shaking
+import { useRouter } from 'next/router'
+import { DashboardNavigation } from 'app/ui/dashboardNavigation'
 
-import { tw } from 'app/design-system/tailwind'
-import { View } from 'app/design-system'
-import Navbar from 'app/ui/navbar'
-import DashboardTabBar from 'app/ui/dashboard-tab-bar'
-
-export const NavigationProvider = ({
+export function NavigationProvider({
   children,
 }: {
   children: React.ReactElement
-}) => {
-  return (
-    <View className="flex flex-1 max-h-[100vh] bg-blue-dark">
-      <Navbar />
-      <View className="flex flex-row flex-1">
-        {tw.prefixMatch('sm') && <DashboardTabBar column />}
-        {children}
-      </View>
-      {!tw.prefixMatch('sm') && <DashboardTabBar />}
-    </View>
-  )
+}) {
+  const router = useRouter()
+  const route = router.route
+  if (route.includes('dashboard')) {
+    return <DashboardNavigation>{children}</DashboardNavigation>
+  }
+  return <>{children}</>
 }
