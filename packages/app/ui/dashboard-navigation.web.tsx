f439cb1b20cf41aa863cfaa903d94f9ed0bbe0c6
co012 <kacper.slusarczyk@swmansion.com>
2022-08-25 20:21:06 +0200

    ADD: web navigation

---
+++ packages/app/ui/dashboard-navigation.web.tsx
@@ -0,0 +1,24 @@
+import { View } from "app/design-system";
+import Navbar from "app/ui/navbar";
+import { tw } from "app/design-system/tailwind";
+import DashboardTabBar from "app/ui/dashboard-tab-bar";
+import React from "react";
+import { useSx } from "dripsy";
+
+export function DashboardNavigation({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  useSx();
+  return (
+    <View className="flex flex-1 max-h-[100vh] bg-blue-dark">
+      <Navbar />
+      <View className="flex flex-row flex-1">
+        {tw.prefixMatch("sm") && <DashboardTabBar column />}
+        {children}
+      </View>
+      {!tw.prefixMatch("sm") && <DashboardTabBar />}
+    </View>
+  );
+}
