228490004c3ec69b504b3c7abbbbe22be2c1a371
co012 <kacper.slusarczyk@swmansion.com>
2022-08-25 19:28:21 +0200

    ADD: dashboards tab bar

---
+++ packages/app/ui/dashboard-tab-bar.tsx
@@ -0,0 +1,37 @@
+import { View } from "app/design-system";
+import Icon from "@expo/vector-icons/MaterialCommunityIcons";
+import { Image, StyleProp, ViewStyle } from "react-native";
+import { Link } from "solito/link";
+import { tw } from "app/design-system/tailwind";
+
+const LinkStyle: StyleProp<ViewStyle> = {
+  flex: 1,
+  flexBasis: 0,
+  padding: 10,
+  alignItems: "center",
+  justifyContent: "center",
+  maxHeight: 128,
+};
+
+export default function DashboardTabBar({ column }: { column?: boolean }) {
+  const rootViewStyle = column
+    ? "flex-col"
+    : "flex-row border-t-2 border-white";
+
+  return (
+    <View className={`flex bg-blue-dark  ${rootViewStyle}`}>
+      <Link viewProps={{ style: LinkStyle }} href={"/dashboard/search"}>
+        <Icon name="magnify" size={32} color="white" />
+      </Link>
+      <Link viewProps={{ style: LinkStyle }} href={"/dashboard/chart"}>
+        <Image
+          style={tw`w-8 h-8 rounded-full border border-white`}
+          source={require("app/assets/images/icon.png")}
+        />
+      </Link>
+      <Link viewProps={{ style: LinkStyle }} href={"/dashboard/profile"}>
+        <Icon name="account-outline" size={32} color="white" />
+      </Link>
+    </View>
+  );
+}
