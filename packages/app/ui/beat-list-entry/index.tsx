1541400eecceb79619e11224d73bf517149f5d8b
co012 <kacper.slusarczyk@swmansion.com>
2022-08-26 17:38:03 +0200

    ADD: top chart

--- packages/app/ui/beat-list-entry/index.tsx
+++ packages/app/ui/beat-list-entry/index.tsx
@@ -3,19 +3,30 @@ import { Entry } from "app/api/graphql";
 import { Image } from "react-native";
 import { imageUrlSmall } from "app/utils/entry";
 import Icon from "@expo/vector-icons/MaterialCommunityIcons";
-import { Price } from "app/features/dashboard/search/recently-added/list-entry/price";
-import { FavoriteButton } from "app/features/dashboard/search/recently-added/list-entry/favoriteButton";
+import { Price } from "app/ui/beat-list-entry/price";
+import { FavoriteButton } from "app/ui/beat-list-entry/favoriteButton";
 import { tw } from "app/design-system/tailwind";
 
-export function BeatListEntry({ entry }: { entry: Entry }) {
+export function BeatListEntry({
+  entry,
+  spot,
+}: {
+  entry: Entry;
+  spot?: number;
+}) {
   return (
-    <View className="w-full flex flex-row py-2">
+    <View className="w-full flex items-center flex-row py-2">
       <Image
         style={{ width: 40, height: 40 }}
         source={{
           uri: entry.imageUrl ? imageUrlSmall(entry.imageUrl) : "",
         }}
       />
+      {spot && (
+        <Text className="text-2xl leading-none text-center ml-2 w-11">
+          {spot}
+        </Text>
+      )}
       <View className="ml-2 flex justify-center">
         <Text className="text-sm font-bold leading-6">{entry.title}</Text>
         <Text className="text-xs text-neutral-400 leading-6">
