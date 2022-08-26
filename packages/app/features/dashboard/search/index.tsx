8e9de2c4a0537829cabeb0b9108066fe3cb7fff7
co012 <kacper.slusarczyk@swmansion.com>
2022-08-26 10:19:37 +0200

    UPDATE: move serch screen

---
+++ packages/app/features/dashboard/search/index.tsx
@@ -0,0 +1,25 @@
+import { SafeAreaView } from "app/design-system/safe-area-view";
+import React, { useState } from "react";
+import { TabBar, Tabs } from "app/features/dashboard/search/tabs";
+import { View } from "app/design-system";
+import { SearchInputField } from "app/features/dashboard/search/search-input-field";
+
+export function SearchScreen() {
+  const [searchFraze, setSearchFraze] = useState("");
+  const [tab, setTab] = useState<Tabs>("Beats");
+
+  return (
+    <SafeAreaView className="w-full max-w-6xl mx-auto flex-1 flex p-4">
+      <SearchInputField
+        value={searchFraze}
+        onChangeText={setSearchFraze}
+        showX={searchFraze !== ""}
+        onXClick={() => {
+          setSearchFraze("");
+        }}
+      />
+      <TabBar selected={tab} onTabClick={setTab} />
+      <View className="flex-1"></View>
+    </SafeAreaView>
+  );
+}
