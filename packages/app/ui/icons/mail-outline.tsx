4b35cf7132d54e7d2226c3a1823e5eb4a5420241
co012 <kacper.slusarczyk@swmansion.com>
2022-08-29 17:05:48 +0200

    FIX: add types for icons

--- packages/app/ui/icons/mail-outline.tsx
+++ packages/app/ui/icons/mail-outline.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import Svg, { Path } from "react-native-svg";
 
-function Icon({ size = 24, color }) {
+function Icon({ size = 24, color = "currentColor" }) {
   return (
     <Svg width={size} height={size} viewBox="0 0 24 24">
       <Path
-        fill={color ? color : "currentColor"}
+        fill={color}
         d="M22 6C22 4.9 21.1 4 20 4H4C2.9 4 2 4.9 2 6V18C2 19.1 2.9 20 4 20H20C21.1 20 22 19.1 22 18V6M20 6L12 11L4 6H20M20 18H4V8L12 13L20 8V18Z"
       />
     </Svg>
