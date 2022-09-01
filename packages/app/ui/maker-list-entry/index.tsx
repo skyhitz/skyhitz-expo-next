d3b13d1d4e5c9124a5f63e9a7dab557a9fe999e4
co012 <kacper.slusarczyk@swmansion.com>
2022-09-01 14:54:39 +0200

    IMPROVE: Beatmaker list entry

--- packages/app/ui/maker-list-entry/index.tsx
+++ packages/app/ui/maker-list-entry/index.tsx
@@ -1,36 +1,17 @@
 import { User } from "app/api/graphql";
 import { Text, View } from "app/design-system";
-import { Image } from "react-native";
-import { getInitials } from "app/utils/user";
-import { tw } from "app/design-system/tailwind";
+import { UserAvatar } from "app/ui/userAvatar";
 
 export function MakerListEntry({ user }: { user: User }) {
   return (
     <View className="w-full flex items-center flex-row py-2">
-      <View className="relative w-8 h-8 bg-blue rounded-full overflow-hidden">
-        <View className="w-8 h-8 flex items-center justify-center">
-          <Text className="text-xs text-black leading-none">
-            {getInitials(user.displayName!)}
-          </Text>
-        </View>
-        {!!user.avatarUrl && (
-          <Image
-            source={{
-              uri: user.avatarUrl!,
-              width: 32,
-              height: 32,
-            }}
-            style={tw`rounded-full absolute`}
-          />
-        )}
-      </View>
-
+      <UserAvatar user={user} size="small" />
       <View>
-        <View className="ml-2 flex justify-center flex-1 pr-2">
-          <Text numberOfLines={1} className="text-xs -tracking-tight leading-5">
+        <View className="px-2 flex justify-center flex-1">
+          <Text numberOfLines={1} className="text-xs font-bold">
             {user.username}
           </Text>
-          <Text numberOfLines={1} className="text-xs -tracking-tight leading-5">
+          <Text numberOfLines={1} className="text-xs mt-1">
             {user.displayName}
           </Text>
         </View>
