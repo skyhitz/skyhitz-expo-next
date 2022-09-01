1ad200c97d060f6599d894a6f4ed77ef965b6766
co012 <kacper.slusarczyk@swmansion.com>
2022-09-01 15:23:30 +0200

    UPDATE: design

--- packages/app/features/dashboard/search/beatmakersEmptyState.tsx
+++ packages/app/features/dashboard/search/beatmakersEmptyState.tsx
@@ -4,8 +4,8 @@ import Search from "app/ui/icons/search";
 export default function BeatmakersEmptyState() {
   return (
     <View className="flex-1 py-8 flex-row justify-center mx-auto">
-      <Search color="white" size={24} />
-      <Text className="ml-1 text-lg">Search for beatmakers</Text>
+      <Search color="white" size={22} />
+      <Text className="ml-3 text-lg">Search for Beatmakers</Text>
     </View>
   );
 }
