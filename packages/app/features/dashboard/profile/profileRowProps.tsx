1375db62397b89769227fcec8660e7089230708a
Kacper Ślusarczyk <kacper.slusarczyk@swmansion.com>
2022-09-01 14:13:59 +0200

    ADD: Profile (#23)
    * ADD: big userAvatar variant
    
    * UPDATE: profile directory
    
    * ADD: profile row component
    
    * ADD: mintNewNftBtn
    
    * ADD: buyXmlBtn
    
    * IMPROVE: profile
    
    * ADD: ellipsis in the middle
    
    * FIX: ellipsis in the middle
    
    * ADD: compile option present in next build
    
    * IMPROVE: review comments part 1
    
    * IMPROVE: change boolean to enum
    
    * IMPROVE: remove magic constant
    
    * IMPROVE: textEllipsis

---
+++ packages/app/features/dashboard/profile/profileRowProps.tsx
@@ -0,0 +1,25 @@
+import { Pressable, Text, View } from "app/design-system";
+import ChevronRight from "app/ui/icons/chevron-right";
+import { ReactElement } from "react";
+import { tw } from "app/design-system/tailwind";
+
+type ProfileRowProps = {
+  icon: ReactElement;
+  title: string;
+  number?: number;
+};
+
+export function ProfileRow({ icon, number, title }: ProfileRowProps) {
+  return (
+    <Pressable className="flex flex-row justify-between py-1.5">
+      <View className="flex flex-row items-center">
+        {icon}
+        <Text className="font-bold ml-2 text-sm">{title}</Text>
+      </View>
+      <View className="flex flex-row items-center">
+        <Text className="text-sm">{number}</Text>
+        <ChevronRight size={28} color={tw.color("white")} />
+      </View>
+    </Pressable>
+  );
+}
---
+++ packages/app/features/dashboard/profile/textEllipsis.tsx
@@ -0,0 +1,50 @@
+import { useEffect, useState } from "react";
+import { insert, join, pipe, remove, split } from "ramda";
+import { Text, View } from "app/design-system";
+import { tw } from "app/design-system/tailwind";
+
+const textEllipsisLength = 2;
+
+export function TextEllipsis({ text }: { text: string }) {
+  const [wrapperWidth, setWrapperWidth] = useState(0);
+  const [displayedText, setDisplayedText] = useState(text);
+  const [charLength, setCharLength] = useState(0);
+
+  useEffect(() => {
+    if (!charLength || !wrapperWidth) return;
+    const availableTextLength = wrapperWidth / charLength;
+    if (availableTextLength >= text.length) {
+      setDisplayedText(text);
+      return;
+    }
+
+    const lengthToCut = text.length - availableTextLength + textEllipsisLength;
+    const cutStart = Math.round(text.length / 2 - lengthToCut / 2);
+    const cutText = pipe(
+      split(""),
+      remove(cutStart, lengthToCut),
+      insert(cutStart, "..."),
+      join("")
+    );
+    setDisplayedText(cutText(text));
+  }, [charLength, wrapperWidth, setDisplayedText, text]);
+
+  return (
+    <View
+      className="flex-1 mx-2.5 flex-row h-3"
+      onLayout={(e) => {
+        setWrapperWidth(e.nativeEvent.layout.width);
+      }}
+    >
+      <Text
+        style={tw.style("text-xs font-bold absolute leading-5")}
+        onLayout={(e) => {
+          if (charLength) return;
+          setCharLength(e.nativeEvent.layout.width / displayedText.length);
+        }}
+      >
+        {displayedText}
+      </Text>
+    </View>
+  );
+}
