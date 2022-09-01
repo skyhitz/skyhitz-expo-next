ed64e1708287fb2b0ba5b58435a33a7cc7a9d4f3
Kacper Ślusarczyk <kacper.slusarczyk@swmansion.com>
2022-09-01 08:29:17 +0200

    ADD: beat info modal (#22)
    * ADD: modal
    
    * IMPROVE: move favoriteButton.tsx
    
    * IMPROVE: price looks
    
    * ADD: show more - interactions
    
    * ADD: show more - description
    
    * ADD: show more - cancelBtn
    
    * ADD: show more
    
    * IMPROVE: review comments
    
    * IMPROVE: remove expo icons

---
+++ packages/app/ui/beat-list-entry/show-more/index.tsx
@@ -0,0 +1,32 @@
+import { Entry } from "app/api/graphql";
+import { useState } from "react";
+import { tw } from "app/design-system/tailwind";
+import { Modal, Pressable } from "app/design-system";
+import { SafeAreaView } from "app/design-system/safe-area-view";
+import { BeatInfo } from "app/ui/beat-list-entry/show-more/beatInfo";
+import { BeatActions } from "app/ui/beat-list-entry/show-more/beatActions";
+import { CancelBtn } from "app/ui/beat-list-entry/show-more/cancelBtn";
+import VerticalDots from "app/ui/icons/verticalDots";
+
+export function ShowMore({ entry }: { entry: Entry }) {
+  const [showing, setShowing] = useState(false);
+  return (
+    <>
+      <Pressable onPress={() => setShowing(!showing)}>
+        <VerticalDots size={30} color={tw.color("white")} />
+      </Pressable>
+
+      <Modal transparent={true} visible={showing} animationType="fade">
+        <SafeAreaView className="bg-blue-field/90 flex-1 items-center justify-around pt-10 sm:pt-20">
+          <BeatInfo
+            imageUrl={entry.imageUrl ?? ""}
+            title={entry.title ?? ""}
+            artist={entry.artist ?? ""}
+          />
+          <BeatActions />
+          <CancelBtn onPress={() => setShowing(false)} />
+        </SafeAreaView>
+      </Modal>
+    </>
+  );
+}
