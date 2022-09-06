fcd5ee7b046aadf4addfc07ebfbcc14546758d78
co012 <kacper.slusarczyk@swmansion.com>
2022-09-06 16:16:16 +0200

    FIX: line component

--- packages/app/ui/orSeparator.tsx
+++ packages/app/ui/orSeparator.tsx
@@ -1,15 +1,17 @@
 import { Text, View } from "app/design-system";
 
-export const Line = () => (
-  <View className="border border-transparent border-b-white grow" />
-);
+export function Line({ className = "w-full" }) {
+  return (
+    <View className={`border border-transparent border-b-white ${className}`} />
+  );
+}
 
 export function Separator() {
   return (
     <View className="flex flex-row my-8 items-center w-full">
-      <Line />
+      <Line className="grow" />
       <Text className="px-2">or</Text>
-      <Line />
+      <Line className="grow" />
     </View>
   );
 }
