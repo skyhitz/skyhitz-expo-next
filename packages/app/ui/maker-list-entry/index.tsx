c3bd58bed6add0a27bfbbc83e0b333073fa0fdaf
co012 <kacper.slusarczyk@swmansion.com>
2022-09-07 10:14:30 +0200

    FIX: merge artifacts

--- packages/app/ui/maker-list-entry/index.tsx
+++ packages/app/ui/maker-list-entry/index.tsx
@@ -5,7 +5,11 @@ import { UserAvatar } from "app/ui/userAvatar";
 export function MakerListEntry({ user }: { user: User }) {
   return (
     <View className="w-full flex items-center flex-row py-2">
-      <UserAvatar user={user} size="small" />
+      <UserAvatar
+        avatarUrl={user.avatarUrl}
+        displayName={user.displayName}
+        size="small"
+      />
       <View>
         <View className="px-2 flex justify-center flex-1">
           <Text numberOfLines={1} className="text-xs font-bold">
