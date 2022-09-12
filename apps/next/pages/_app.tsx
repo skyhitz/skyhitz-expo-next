de6d344c49eb64f0b6c648e2822f8b515a94ea71
Kacper Ślusarczyk <kacper.slusarczyk@swmansion.com>
2022-09-12 12:35:54 +0200

    ADD: toasts (#29)
    * ADD: toast dependencies
    
    * ADD: toast skeleton
    
    * IMPROVE: toast
    
    * IMPROVE: toast style
    
    * IMPROVE: review comment
    
    * IMPROVE: web toast look

--- apps/next/pages/_app.tsx
+++ apps/next/pages/_app.tsx
@@ -4,6 +4,8 @@ import React from "react";
 import type { SolitoAppProps } from "solito";
 import "raf/polyfill";
 import { WebNavigation } from "app/navigation/web";
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
 
 function MyApp({ Component, pageProps }: SolitoAppProps) {
   return (
@@ -20,6 +22,12 @@ function MyApp({ Component, pageProps }: SolitoAppProps) {
         <WebNavigation>
           <Component {...pageProps} />
         </WebNavigation>
+        <ToastContainer
+          position="bottom-right"
+          hideProgressBar={true}
+          theme="dark"
+          closeButton={false}
+        />
       </Provider>
     </>
   );
