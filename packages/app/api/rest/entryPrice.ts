953fec21f03c670324a1b95c121a963c878afe7e
co012 <kacper.slusarczyk@swmansion.com>
2022-09-14 16:14:29 +0200

    ADD: request for price

---
+++ packages/app/api/rest/entryPrice.ts
@@ -0,0 +1,15 @@
+import { Config } from "app/config";
+
+export async function getEntryPrice(code: string, issuer: string) {
+  const response = await fetch(
+    `${Config.HORIZON_URL}/order_book?selling_asset_type=credit_alphanum12&selling_asset_code=${code}&selling_asset_issuer=${issuer}&buying_asset_type=native`
+  );
+  const { asks } = await response.json();
+  console.log(code, issuer);
+  if (asks && asks[0]) {
+    const { price, amount }: { price: string; amount: string } = asks[0];
+    return { price: parseFloat(price), amount: parseFloat(amount) };
+  }
+
+  throw "Unknown error";
+}
