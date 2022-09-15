81b8ccca5650ce17111ddafb45dae73c4f603b36
co012 <kacper.slusarczyk@swmansion.com>
2022-09-15 08:57:29 +0200

    IMPROVE: don't throw error when no price present

--- packages/app/api/rest/entryPrice.ts
+++ packages/app/api/rest/entryPrice.ts
@@ -5,11 +5,13 @@ export async function getEntryPrice(code: string, issuer: string) {
     `${Config.HORIZON_URL}/order_book?selling_asset_type=credit_alphanum12&selling_asset_code=${code}&selling_asset_issuer=${issuer}&buying_asset_type=native`
   );
   const { asks } = await response.json();
-  console.log(code, issuer);
-  if (asks && asks[0]) {
-    const { price, amount }: { price: string; amount: string } = asks[0];
-    return { price: parseFloat(price), amount: parseFloat(amount) };
+  if (asks) {
+    if (asks[0]) {
+      const { price, amount }: { price: string; amount: string } = asks[0];
+      return { price: parseFloat(price), amount: parseFloat(amount) };
+    }
+    return { price: 0, amount: 0 };
   }
 
-  throw "Unknown error";
+  throw { message: "Unknown error" };
 }
