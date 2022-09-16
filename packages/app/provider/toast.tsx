29a65841bca8064b13f7c761765c8b154a4a3cb9
co012 <kacper.slusarczyk@swmansion.com>
2022-09-16 15:21:51 +0200

    FIX: wrong useSafeArea

--- packages/app/provider/toast.tsx
+++ packages/app/provider/toast.tsx
@@ -1,14 +1,14 @@
 import { ToastProvider } from "react-native-toast-notifications";
 import { ReactElement } from "react";
 import { Toast } from "app/ui/toast";
-import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { useSafeArea } from "app/provider/safe-area/useSafeArea";
 
 export default function SkyhitzToastProvider({
   children,
 }: {
   children: ReactElement;
 }) {
-  const { top } = useSafeAreaInsets();
+  const { top } = useSafeArea();
   return (
     <ToastProvider placement="top" renderToast={Toast} offsetTop={top}>
       {children}
