204da1dd9f9e95a09df7d649d113ab7bc9556f22
co012 <kacper.slusarczyk@swmansion.com>
2022-09-19 14:47:06 +0200

    FIX: wrong icon

--- packages/app/features/dashboard/search/searchInputField.tsx
+++ packages/app/features/dashboard/search/searchInputField.tsx
@@ -2,9 +2,9 @@ import React, { ForwardedRef } from "react";
 import { TextInput, TextProps } from "app/design-system/textInput";
 import { TextInput as rTextInput } from "react-native";
 import { Pressable, View } from "app/design-system";
-import Icon from "@expo/vector-icons/MaterialCommunityIcons";
 import { tw } from "app/design-system/tailwind";
 import Search from "app/ui/icons/search";
+import X from "app/ui/icons/x";
 
 export const SearchInputField = React.forwardRef(function SearchInputField(
   {
@@ -16,7 +16,7 @@ export const SearchInputField = React.forwardRef(function SearchInputField(
 ) {
   return (
     <View className="w-full bg-white rounded-lg px-2 py-1 flex flex-row">
-      <Icon name="magnify" color={tw.color("black")} size={24} />
+      <Search color={tw.color("black")} size={24} />
       <TextInput
         placeholder="Search"
         placeholderTextColor="black"
@@ -26,7 +26,7 @@ export const SearchInputField = React.forwardRef(function SearchInputField(
       />
       {showX && (
         <Pressable onPress={() => onXClick?.call(null)}>
-          <Search color={tw.color("black")} size={24} />
+          <X color={tw.color("black")} size={24} />
         </Pressable>
       )}
     </View>
