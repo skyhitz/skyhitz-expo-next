0f369aebc4ad563631235386b47202616019efff
co012 <kacper.slusarczyk@swmansion.com>
2022-09-20 14:40:40 +0200

    ADD: getUser algolia request

--- packages/app/api/algolia.ts
+++ packages/app/api/algolia.ts
@@ -1,7 +1,19 @@
 import { Config } from "app/config";
 import algoliasearch from "algoliasearch/lite";
+import { PublicUser } from "app/api/graphql";
 
 const appDomain = Config.APP_URL.replace("https://", "");
 const client = algoliasearch(Config.ALGOLIA_APP_ID, Config.ALGOLIA_SEARCH_KEY);
 export const entriesIndex = client.initIndex(`${appDomain}:entries`);
 export const usersIndex = client.initIndex(`${appDomain}:users`);
+
+export async function getUser(id: string): Promise<PublicUser> {
+  const res = await usersIndex.search("", {
+    filters: `id:${id}`,
+  });
+  if (res.hits.length === 0) {
+    throw "User not found";
+  }
+  const [user] = res.hits;
+  return user as PublicUser;
+}
