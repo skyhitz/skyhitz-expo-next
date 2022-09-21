7183302eda740699eb45474a3649631aaff31035
Kacper Ślusarczyk <kacper.slusarczyk@swmansion.com>
2022-09-21 10:41:34 +0200

    FEATURE: Beatmaker profile page (#38)
    * UPDATE: move hook
    
    * ADD: useBeatmakerParam hook
    
    * ADD: new color
    
    * ADD: beatmaker screen
    
    * ADD: linking to beatmaker page
    
    * ADD: redirect to beatmaker screen on press
    
    * FIX: missing prop
    
    * IMPROVE: use skip
    
    * IMPROVE: add header
    
    * IMPROVE: use dynamic route
    
    * IMPROVE: put beatmakerScreen into stack navigation
    
    I hate it
    
    * Revert "IMPROVE: put beatmakerScreen into stack navigation"
    
    This reverts commit fbeabb9800052d63d1044b0a2701a896a373fce2.
    
    * ADD: searchStackNavigation and place beatmaker there
    
    * ADD: getUser algolia request
    
    * ADD: useErrorReport.ts
    
    * FIX: getUser algolia request
    
    * IMPROVE: use only id parameter
    
    * IMPROVE: make user const
    
    * IMPROVE: remove leftover
    
    * IMPROVE: change beatmaker link
    
    * ADD: useUserWithId
    
    * FIX: type error

--- packages/app/ui/maker-list-entry/index.tsx
+++ packages/app/ui/maker-list-entry/index.tsx
@@ -1,25 +1,30 @@
 import { User } from "app/api/graphql";
-import { Text, View } from "app/design-system";
+import { Pressable, Text, View } from "app/design-system";
 import { UserAvatar } from "app/ui/userAvatar";
+import { useRouter } from "solito/router";
 
 export function MakerListEntry({ user }: { user: User }) {
+  const { push } = useRouter();
+
   return (
-    <View className="w-full flex items-center flex-row py-2">
-      <UserAvatar
-        avatarUrl={user.avatarUrl}
-        displayName={user.displayName}
-        size="small"
-      />
-      <View>
-        <View className="px-2 flex justify-center flex-1">
-          <Text numberOfLines={1} className="text-xs font-bold">
-            {user.username}
-          </Text>
-          <Text numberOfLines={1} className="text-xs mt-1">
-            {user.displayName}
-          </Text>
+    <Pressable onPress={() => push(`/dashboard/search/beatmaker/${user.id}/`)}>
+      <View className="w-full flex items-center flex-row py-2">
+        <UserAvatar
+          avatarUrl={user.avatarUrl}
+          displayName={user.displayName}
+          size="small"
+        />
+        <View>
+          <View className="px-2 flex justify-center flex-1">
+            <Text numberOfLines={1} className="text-xs font-bold">
+              {user.username}
+            </Text>
+            <Text numberOfLines={1} className="text-xs mt-1">
+              {user.displayName}
+            </Text>
+          </View>
         </View>
       </View>
-    </View>
+    </Pressable>
   );
 }
