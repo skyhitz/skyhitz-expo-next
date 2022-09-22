b93dc2a937b99c7c474e08c6b4aff0a3483cf32d
co012 <kacper.slusarczyk@swmansion.com>
2022-09-22 16:02:29 +0200

    IMPROVE: beat page

--- packages/app/features/dashboard/beat/index.tsx
+++ packages/app/features/dashboard/beat/index.tsx
@@ -1,5 +1,18 @@
-import { View } from "app/design-system";
+import { useBeatParam } from "app/hooks/param/useBeatParam";
+import { tw } from "app/design-system/tailwind";
+import DesktopView from "app/features/dashboard/beat/desktopView";
+import MobileView from "app/features/dashboard/beat/mobileView";
+import * as assert from "assert";
+import { useSx } from "dripsy";
 
 export default function BeatScreen() {
-  return <View></View>;
+  const id = useBeatParam();
+  assert.ok(id);
+  useSx();
+
+  return tw.prefixMatch("md") ? (
+    <DesktopView id={id} />
+  ) : (
+    <MobileView id={id} />
+  );
 }
