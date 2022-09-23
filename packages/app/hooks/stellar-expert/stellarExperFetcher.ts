20e4e8cbd4502a310e67fae914868f1981d3bf38
co012 <kacper.slusarczyk@swmansion.com>
2022-09-23 15:06:18 +0200

    FIX: type

--- packages/app/hooks/stellar-expert/stellarExperFetcher.ts
+++ packages/app/hooks/stellar-expert/stellarExperFetcher.ts
@@ -1,20 +1,20 @@
 import { Config } from "app/config";
 
-type ResponseTypeBase = {
+interface ResponseTypeBase {
   _links: unknown;
   _embedded: {
     _records: object[];
   };
-};
+}
 
 export default async function fetcher<ResponseT extends ResponseTypeBase>(
   assetId: string,
   endpoint: string
-) {
+): Promise<ResponseT["_embedded"]["_records"]> {
   const url = `${Config.STELLAR_EXPERT_URL}/asset/${assetId}/${endpoint}`;
   const result = await fetch(url);
   const json = await result.json();
-  const data = json as ResponseT;
+  const data: ResponseT = json as ResponseT;
   if (!data) {
     console.log("Unknown response", json);
     throw new Error("Unknown response");
