d6d077f6860146a1c4b3e06aaf7f0257d2716380
co012 <kacper.slusarczyk@swmansion.com>
2022-09-23 13:52:48 +0200

    IMPROVE: activity.tsx

--- packages/app/features/dashboard/beat/activity.tsx
+++ packages/app/features/dashboard/beat/activity.tsx
@@ -1,5 +1,55 @@
 import { CollapsableView } from "app/features/dashboard/beat/collapsableView";
+import useAssetHistory from "app/hooks/stellar-expert/useAssetHistory";
+import { Maybe } from "app/types";
+import { Text, View } from "app/design-system";
 
-export function Activity() {
-  return <CollapsableView headerText="Activity" />;
+const typeNumberMeaning: Record<number, string> = {
+  0: "Account created",
+  1: "Transferred to",
+  2: "Transferred dimmed",
+  3: "Placed a new offer or modified existing",
+  6: "Established trustline",
+  13: "Transferred",
+};
+
+function ActivityEntry({
+  date,
+  operation,
+  type,
+}: {
+  date: Date;
+  operation: string;
+  type: number;
+}) {
+  return (
+    <View className="flex flex-row justify-between my-0.5">
+      <Text>{operation}</Text>
+      <Text>{typeNumberMeaning[type] ?? "Unknown activity"}</Text>
+      <Text>{date}</Text>
+    </View>
+  );
+}
+
+export function Activity({
+  code,
+  issuer,
+}: {
+  code: Maybe<string>;
+  issuer: Maybe<string>;
+}) {
+  const { data } = useAssetHistory(code, issuer);
+  return (
+    <CollapsableView headerText="Activity">
+      <View className="mx-5 my-4.5">
+        {data?.map((item) => (
+          <ActivityEntry
+            key={item.id}
+            date={new Date(item.ts)}
+            operation={item.id}
+            type={item.type}
+          />
+        )) ?? <Text>No activity</Text>}
+      </View>
+    </CollapsableView>
+  );
 }
---
+++ packages/app/hooks/stellar-expert/useAssetHistory.ts
@@ -0,0 +1,52 @@
+import { Maybe } from "app/types";
+import { getAssetId } from "app/utils/stellar";
+import useSWR from "swr";
+import { Config } from "app/config";
+
+type ResponseType = {
+  _links: unknown;
+  _embedded: {
+    _records: {
+      id: string;
+      paging_token: string;
+      type: number;
+      ts: number;
+      accounts: string[];
+      ledger: number;
+      tx: string;
+      assets: string[];
+      source_amount?: string;
+      offer?: string;
+      price?: {
+        n: number;
+        d: number;
+      };
+      createdOffer?: string;
+      amount?: string;
+      dest_min?: string;
+      memo?: string;
+      memo_type?: number;
+    }[];
+  };
+};
+
+const fetcher = async (assetId: string) => {
+  const url = `${Config.STELLAR_EXPERT_URL}/asset/${assetId}/history/all`;
+  const result = await fetch(url);
+  const json = await result.json();
+  const data = json as ResponseType;
+  if (!data) {
+    console.log("Unknown response", json);
+    throw new Error("Unknown response");
+  }
+  return data._embedded._records;
+};
+
+export default function useAssetHistory(
+  code: Maybe<string>,
+  issuer: Maybe<string>
+) {
+  const id = getAssetId(code, issuer);
+  const result = useSWR(id, id ? fetcher : null);
+  return result;
+}
