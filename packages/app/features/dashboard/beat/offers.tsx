8dbb92afc8eab14e9123b12bc9b5e5321d71f4d1
co012 <kacper.slusarczyk@swmansion.com>
2022-09-23 13:53:01 +0200

    IMPROVE: offers.tsx

--- packages/app/features/dashboard/beat/offers.tsx
+++ packages/app/features/dashboard/beat/offers.tsx
@@ -1,5 +1,54 @@
 import { CollapsableView } from "app/features/dashboard/beat/collapsableView";
+import { Text, View } from "app/design-system";
+import { Maybe } from "app/types";
+import useAssetOffers from "app/hooks/stellar-expert/useAssetOffers";
 
-export function Offers() {
-  return <CollapsableView headerText="Offers" />;
+const typeNumberMeaning: Record<number, string> = {
+  0: "Account created",
+  1: "Transferred to",
+  2: "Transferred dimmed",
+  3: "Placed a new offer or modified existing",
+  6: "Established trustline",
+  13: "Transferred",
+};
+
+function OfferEntry({
+  date,
+  operation,
+  type,
+}: {
+  date: Date;
+  operation: string;
+  type: number;
+}) {
+  return (
+    <View className="flex flex-row justify-between my-0.5">
+      <Text>{operation}</Text>
+      <Text>{typeNumberMeaning[type] ?? "Unknown activity"}</Text>
+      <Text>{date}</Text>
+    </View>
+  );
+}
+export function Offers({
+  code,
+  issuer,
+}: {
+  code: Maybe<string>;
+  issuer: Maybe<string>;
+}) {
+  const { data } = useAssetOffers(code, issuer);
+  return (
+    <CollapsableView headerText="Offers">
+      <View className="mx-5 my-4.5">
+        {data?.map((item) => (
+          <OfferEntry
+            key={item.id}
+            date={new Date(item.ts)}
+            operation={item.id}
+            type={item.type}
+          />
+        )) ?? <Text>No offers</Text>}
+      </View>
+    </CollapsableView>
+  );
 }
