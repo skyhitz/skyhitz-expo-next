f634f14c45586493ef42ba3edc29755a11a5343d
co012 <kacper.slusarczyk@swmansion.com>
2022-09-23 11:15:31 +0200

    IMPROVE: beat description

--- packages/app/features/dashboard/beat/description.tsx
+++ packages/app/features/dashboard/beat/description.tsx
@@ -1,21 +1,12 @@
 import { CollapsableView } from "app/features/dashboard/beat/collapsableView";
 import { Text } from "app/design-system";
+import { Maybe } from "app/types";
+import InfoCircle from "app/ui/icons/info-circle";
 
-export function Description() {
+export function Description({ description }: { description: Maybe<string> }) {
   return (
-    <CollapsableView initCollapsed={false} headerText="Description">
-      <Text className="p-3">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut tristique
-        vitae tortor mattis lobortis. Pellentesque mattis ex sodales sapien
-        dapibus mattis. Vivamus at ipsum aliquet, fermentum quam quis, pulvinar
-        nunc. Nullam sagittis vestibulum imperdiet. Lorem ipsum dolor sit amet,
-        consectetur adipiscing elit. Quisque eleifend ac elit sed finibus.
-        Quisque consequat dictum pulvinar. Fusce facilisis vitae arcu ac rutrum.
-        Maecenas augue orci, rhoncus eget lorem ac, finibus condimentum nisi.
-        Praesent cursus, ipsum nec varius consequat, eros arcu ultricies velit,
-        eleifend ullamcorper leo nulla at erat. Vestibulum at arcu egestas,
-        accumsan lectus eu, bibendum lacus.
-      </Text>
+    <CollapsableView icon={InfoCircle} headerText="Description">
+      <Text className="p-3">{description}</Text>
     </CollapsableView>
   );
 }
