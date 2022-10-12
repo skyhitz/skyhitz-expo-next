94f541417d6444f8a4fa981c56c848a3122afcf9
Jakub Mysliwiec <jakub.mysliwiec@swmansion.com>
2022-10-12 12:47:03 +0200

    removed expo-device dependency

--- packages/app/navigation/native/index.tsx
+++ packages/app/navigation/native/index.tsx
@@ -8,6 +8,8 @@ import TermsScreen from "app/features/legal/termsScreen";
 import PrivacyScreen from "app/features/legal/privacyScreen";
 import { tw } from "app/design-system/tailwind";
 import { useAuthStatus } from "app/hooks/useAuthStatus";
+import { useRecoilValue } from "recoil";
+import { appInitializedAtom } from "app/state/user";
 
 const Stack = createNativeStackNavigator<{
   splash: undefined;
@@ -21,6 +23,8 @@ const Stack = createNativeStackNavigator<{
 }>();
 
 export function NativeNavigation() {
+  const initialized = useRecoilValue(appInitializedAtom);
+
   useAuthStatus();
 
   return (
@@ -29,76 +33,81 @@ export function NativeNavigation() {
         headerShown: false,
       }}
     >
-      <Stack.Screen
-        name="splash"
-        component={SplashScreen}
-        options={{
-          title: "Splash",
-        }}
-      />
+      {!initialized ? (
+        <Stack.Screen
+          name="splash"
+          component={SplashScreen}
+          options={{
+            title: "Splash",
+          }}
+        />
+      ) : (
+        <>
+          {}
+          <Stack.Screen
+            name="home"
+            component={HomeScreen}
+            options={{
+              title: "Home",
+            }}
+          />
 
-      <Stack.Screen
-        name="dashboard"
-        component={DashboardNavigation}
-        options={{
-          title: "Dashboard",
-        }}
-      />
+          <Stack.Screen
+            name="dashboard"
+            component={DashboardNavigation}
+            options={{
+              title: "Dashboard",
+            }}
+          />
 
-      <Stack.Screen
-        name="home"
-        component={HomeScreen}
-        options={{
-          title: "Home",
-        }}
-      />
-
-      <Stack.Screen
-        name="sign-in"
-        component={SignIn}
-        options={{
-          title: "Log In",
-        }}
-      />
-      <Stack.Screen
-        name="sign-up"
-        component={SignUp}
-        options={{
-          title: "Sign Up",
-        }}
-      />
-      <Stack.Screen
-        name="terms"
-        component={TermsScreen}
-        options={{
-          title: "Terms of Use",
-          headerBackTitleVisible: false,
-          headerShown: true,
-          headerStyle: {
-            backgroundColor: tw.color("blue-dark"),
-          },
-          headerTintColor: tw.color("white"),
-          headerTitleStyle: {
-            fontWeight: "bold",
-          },
-        }}
-      />
-      <Stack.Screen
-        name="privacy"
-        component={PrivacyScreen}
-        options={{
-          title: "Privacy Policy",
-          headerBackTitleVisible: false,
-          headerShown: true,
-          headerStyle: {
-            backgroundColor: tw.color("blue-dark"),
-          },
-          headerTintColor: tw.color("white"),
-          headerTitleStyle: {
-            fontWeight: "bold",
-          },
-        }}
-      />
+          <Stack.Screen
+            name="sign-in"
+            component={SignIn}
+            options={{
+              title: "Log In",
+            }}
+          />
+          <Stack.Screen
+            name="sign-up"
+            component={SignUp}
+            options={{
+              title: "Sign Up",
+            }}
+          />
+          <Stack.Screen
+            name="terms"
+            component={TermsScreen}
+            options={{
+              title: "Terms of Use",
+              headerBackTitleVisible: false,
+              headerShown: true,
+              headerStyle: {
+                backgroundColor: tw.color("blue-dark"),
+              },
+              headerTintColor: tw.color("white"),
+              headerTitleStyle: {
+                fontWeight: "bold",
+              },
+            }}
+          />
+          <Stack.Screen
+            name="privacy"
+            component={PrivacyScreen}
+            options={{
+              title: "Privacy Policy",
+              headerBackTitleVisible: false,
+              headerShown: true,
+              headerStyle: {
+                backgroundColor: tw.color("blue-dark"),
+              },
+              headerTintColor: tw.color("white"),
+              headerTitleStyle: {
+                fontWeight: "bold",
+              },
+            }}
+          />
+        </>
+      )}
     </Stack.Navigator>
   );
 }
