4eefb932c7ca1b37359772349ac621a5ae649ec8
Jakub Mysliwiec <jakub.mysliwiec@swmansion.com>
2022-10-18 19:20:59 +0200

    changed metadata

--- apps/next/pages/_app.tsx
+++ apps/next/pages/_app.tsx
@@ -22,36 +22,21 @@ function MyApp({ Component, pageProps }: SolitoAppProps) {
           name="keywords"
           content="stellar, xlm, mfts, crypto, music nfts, beats market, beats website, lofi beats, fresh beats, music creators, exclusive beats for sale"
         />
-        <meta name="twitter:card" content="app" />
+        <meta name="twitter:card" content="media" />
         <meta name="twitter:site" content="@skyhitzio" />
-        <meta
-          name="twitter:description"
-          content="Upload, buy or sell music NFTs on the Stellar Network. Join a music community of beatmakers!"
-        />
-        <meta name="twitter:app:country" content="US" />
-        <meta name="twitter:app:name:iphone" content="Skyhitz" />
-        <meta name="twitter:app:id:iphone" content="1105406020" />
-        <meta name="twitter:app:url:iphone" content="skyhitz://+" />
-        <meta name="twitter:app:name:ipad" content="Skyhitz" />
-        <meta name="twitter:app:id:ipad" content="1105406020" />
-        <meta name="twitter:app:url:ipad" content="skyhitz://+" />
-        <meta name="twitter:app:name:googleplay" content="Skyhitz" />
-        <meta name="twitter:app:id:googleplay" content="com.skyhitz.skyhitz" />
-        <meta
-          name="twitter:app:url:googleplay"
-          content="https://play.google.com/store/apps/details?id=com.skyhitz.skyhitz"
-        />
-        <meta property="og:title" content="Skyhitz" />
+        <meta property="og:title" content="Skyhitz" key="og:title" />
         <meta
           property="og:description"
+          key="og:description"
           content="Upload, buy or sell music NFTs on the Stellar Network. Join a music community of beatmakers!"
         />
-        <meta property="og:type" content="website" />
+        <meta property="og:type" key="og:type" content="website" />
         <meta
           property="og:image"
-          content="https://res.cloudinary.com/skyhitz/image/upload/c_scale,h_531,q_auto/v1583723774/web/social.png"
+          key="og:image"
+          content="https://skyhitz.io/skyhitz.png"
         />
-        <meta property="og:url" content="https://skyhitz.io" />
+        <meta property="og:url" key="og:url" content="https://skyhitz.io" />
         <meta property="fb:app_id" content="564403243666491" />
         <meta property="og:site_name" content="Skyhitz" />
         <meta
--- apps/next/pages/dashboard/beat/[id].tsx
+++ apps/next/pages/dashboard/beat/[id].tsx
@@ -1,6 +1,30 @@
 import React from "react";
 import BeatScreen from "app/features/dashboard/beat";
+import Head from "next/head";
 
 export default function BeatPage() {
-  return <BeatScreen />;
+  return (
+    <>
+      <Head>
+        <meta property="og:title" content="Beat" key="og:title" />
+        <meta
+          property="og:description"
+          key="og:description"
+          content="Listen to the uploaded beat!"
+        />
+        <meta property="og:type" key="og:type" content="video.other" />
+        <meta
+          property="og:image"
+          key="og:image"
+          content="https://skyhitz.io/skyhitz.png"
+        />
+        <meta
+          property="og:url"
+          key="og:url"
+          content="https://ipfs.io/ipfs/bafybeiaq5om3sg7fund5azcjs4eokaotoidlg5lxyc7pjkljuosiivanga"
+        />
+      </Head>
+      <BeatScreen />
+    </>
+  );
 }
