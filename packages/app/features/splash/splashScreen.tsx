f39079fa51c361ed385bf8e992ff623884a34b2b
Jakub Myśliwiec <48885911+jmysliv@users.noreply.github.com>
2022-10-18 18:57:41 +0200

    FIX: web issues (#62)
    fixed web issues

--- packages/app/features/splash/splashScreen.tsx
+++ packages/app/features/splash/splashScreen.tsx
@@ -1,5 +1,9 @@
-import BackgroundImage from "app/ui/backgroundImage";
+import { ActivityIndicator, View } from "app/design-system";
 
 export function SplashScreen() {
-  return <BackgroundImage />;
+  return (
+    <View className="bg-blue-dark h-full w-full items-center justify-center flex flex-1">
+      <ActivityIndicator />
+    </View>
+  );
 }
