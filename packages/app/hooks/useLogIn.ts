5a53f1bb8208f993003d5730bdc09c8858daae4d
Jakub Myśliwiec <48885911+jmysliv@users.noreply.github.com>
2022-11-07 17:06:45 +0100

    FIX: Authenication infinite loop (#73)
    Fixed authentication view loop

--- packages/app/hooks/useLogIn.ts
+++ packages/app/hooks/useLogIn.ts
@@ -3,18 +3,22 @@ import { userAtom } from "app/state/user";
 import { SecureStorage } from "app/utils/secure-storage";
 import { useRouter } from "solito/router";
 import { User } from "app/api/graphql";
+import { useCallback } from "react";
 
 export function useLogIn(): (user: User) => void {
   const setUserData = useSetRecoilState(userAtom);
   const { push } = useRouter();
 
-  const logIn = async (user: User) => {
-    setUserData(user);
-    if (user.jwt) {
-      await SecureStorage.save("token", user.jwt!);
-    }
-    push("/dashboard/search");
-  };
+  const logIn = useCallback(
+    async (user: User) => {
+      setUserData(user);
+      if (user.jwt) {
+        await SecureStorage.save("token", user.jwt!);
+      }
+      push("/dashboard/search");
+    },
+    [setUserData, push]
+  );
 
   return logIn;
 }
