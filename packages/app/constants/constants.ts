52fce7fad7d06a9910b3a67cf1fa8208a5df4bee
Jakub Myśliwiec <48885911+jmysliv@users.noreply.github.com>
2022-11-07 17:01:43 +0100

    FEATURE: Pinata for videos (#72)
    * Added pinata gateway for videos
    
    * rename const

--- packages/app/constants/constants.ts
+++ packages/app/constants/constants.ts
@@ -2,9 +2,8 @@ export const preBase64String = "data:image/jpeg;base64,";
 export const preBase64StringVideo = "data:video/mp4;base64,";
 export const cloudinaryPreset = "ed0xgbq5";
 export const nftStorageApi = "https://api.nft.storage";
-export const pinataGateway = "https://cyan-absolute-ant-477.mypinata.cloud";
-export const imagesGateway = "https://ipfs.io/ipfs";
-export const videosGateway = "https://ipfs.io/ipfs";
-export const ipfsFallback = "https://cloudflare-ipfs.com/ipfs";
+export const pinataGateway =
+  "https://cyan-absolute-ant-477.mypinata.cloud/ipfs";
+export const fallbackGateway = "https://ipfs.io/ipfs";
 export const ipfsProtocol = "ipfs://";
 export const userDataKey = "sk:user";
