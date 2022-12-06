52701142f14aa4470d781ba9d827290a1ef6000b
Jakub Mysliwiec <jakub.mysliwiec@swmansion.com>
2022-12-06 10:10:31 +0100

    Fixed permission blocking edit profile

--- packages/app/hooks/useMediaLibraryPermission.ts
+++ packages/app/hooks/useMediaLibraryPermission.ts
@@ -1,23 +1,39 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Alert, Platform } from "react-native";
 import { useRouter } from "solito/router";
 import { requestMediaLibraryPermissionsAsync } from "expo-image-picker";
 
-export default function useMediaLibraryPermission() {
+type Props = {
+  showAlert?: boolean;
+};
+
+type Result = {
+  permissionGranted: boolean;
+};
+
+export default function useMediaLibraryPermission(options?: Props): Result {
   const { back } = useRouter();
+  const [permissionGranted, setPermissionGranted] = useState<boolean>(false);
+
   useEffect(() => {
     const getPermissionAsync = async () => {
       const permissions = await requestMediaLibraryPermissionsAsync();
-      if (!permissions.granted) {
+      if (!permissions.granted && options?.showAlert) {
         Alert.alert(
           "Media Library Permission",
           "We need madia library permissions so you can upload beats!",
           [{ text: "OK", onPress: () => back() }]
         );
+      } else if (permissions.granted) {
+        setPermissionGranted(true);
       }
     };
     if (Platform.OS !== "web") {
       getPermissionAsync();
+    } else {
+      setPermissionGranted(true);
     }
   }, [back]);
+
+  return { permissionGranted };
 }
